53c655c00b9f4ff5f4dd53199a97258cb7ae97ca
avivbarka <avivbarkai09@gmail.com>
2024-07-09 21:29:43 +0300

    fix(robot.java) i fixed the first code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,28 +1,49 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-package frc.robot;
-import edu.wpi.first.wpilibj.TimedRobot;
-public class Robot extends TimedRobot {
 
-/**n
- *  is configured to automatically run this class, and to call the functions corresponding to
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+
+/**
+ * he VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
  * the package after creating this project, you must also update the build.gradle file in the
  * project.
-//  
+ */
 public class Robot extends TimedRobot {
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
+    private Joystick driverJoystick;
+    private TalonSRX motor;
     @Override
     public void robotInit() {
-        
+        motor = new TalonSRX(1);
+        driverJoystick = new Joystick(0);
+        motor.setNeutralMode(NeutralMode.Brake);
     }
 
     @Override
-    public void robotPeriodic() {}
+    public void robotPeriodic() {
+        // motor.set(ControlMode.PercentOutput, 0.3);
+
+        if(driverJoystick.getRawButton(1)) {
+            motor.set(ControlMode.PercentOutput, 0.3);
+        } else if(driverJoystick.getRawButton(2)){
+            motor.set(ControlMode.PercentOutput, -0.3);
+        } else {
+            motor.setNeutralMode(NeutralMode.Brake);
+        }
+
+        
+
+
+    }
+
 
     @Override
     public void autonomousInit() {}
@@ -34,9 +55,7 @@ public class Robot extends TimedRobot {
     public void teleopInit() {}
 
     @Override
-    public void teleopPeriodic() {
-
-    }
+    public void teleopPeriodic() {}
 
     @Override
     public void disabledInit() {}
