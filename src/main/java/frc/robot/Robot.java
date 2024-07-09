20c6ccfa8d553562efd6a2e9d5c07611c4dc1a5d
avivbarka <avivbarkai09@gmail.com>
2024-07-09 16:33:28 +0300

    i did the first assignment you asked us to do

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,24 +1,41 @@
+
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
 
+
+
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.PS4Controller.Button;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj.Joystick;
+
+public class Robot extends TimedRobot{
+  private Joystick joystick;
+  private TalonSRX motor;
+  
 
 /**
- * he VM is configured to automatically run this class, and to call the functions corresponding to
+ *  is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
  * the package after creating this project, you must also update the build.gradle file in the
  * project.
- */
+//  
 public class Robot extends TimedRobot {
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
   @Override
-  public void robotInit() {}
+  public void robotInit() {
+    joystick = new Joystick(0);
+    motor = new TalonSRX(1);
+
+  }
 
   @Override
   public void robotPeriodic() {}
@@ -33,7 +50,16 @@ public class Robot extends TimedRobot {
   public void teleopInit() {}
 
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    double outputValue;    
+    int Button;
+    if(joystick.getRawButton(Button:1)){
+      motor.set(ControlMode.PercentOutput, outputValue: 0.3);
+    }
+    if(joystick.getRawButton(Button:2)){
+      motor.setInverted(invert: true);    
+    }
+  }
 
   @Override
   public void disabledInit() {}
